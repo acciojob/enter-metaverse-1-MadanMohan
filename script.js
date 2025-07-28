1cf499c67c8d93206d92e5d09e188fc7cd7c78a0
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-07-28 12:39:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+	const status = document.createElement("p");
+	status.id = "status";
+	status.textContent = "Enter the Metaverse";
+	
+	const button = document.createElement("button");
+	button.id = "enterBtn";
+	button.textContent = "Enter";
+
+document.body.appendChild(status);
+document.body.appendChild(button);
+
+button.addEventListener("click",()=>{
+	status.innerHTML = "<h1>Entered Metaverse</h1>";
+})
